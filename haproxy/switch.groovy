24fe013eb4673f5ff7a1828b9ec1c660ff9f8d3b
Artem V. Ageev <artem.v.ageev@gmail.com>
2023-02-17 10:29:06 +0300

    goroutines

--- haproxy/switch.groovy
+++ haproxy/switch.groovy
@@ -2,30 +2,20 @@ import groovy.json.JsonOutput
 properties([
     disableConcurrentBuilds(),
     parameters([
-        choice(name: 'PROTO', choices: ['http'], description: 'Протокол'),
-        text(name: 'TARGETS', defaultValue: '127.0.0.1', description: 'Цели тестирования'),
-        string(name: 'PORT', defaultValue: '80', trim: true, description: 'Порт'),
-        string(name: 'CONCURRENT', defaultValue: '8', trim: true, description: 'Количество параллельных потоков'),
-        text(name: 'ENDPOINTS', defaultValue: '/', description: 'Endpoints')
+        choice(name: 'ENV', choices: ['test', 'prod'], description: 'Контур'),
+        choice(name: 'TARGET', choices: ['site', 'api', 'elastic'], description: 'Сервис'),
+        choice(name: 'SWITCH', choices: ['off', 'on'], description: 'action')
     ])
 ])
+final Map ENV = [
+    'PROD':'192.168.25.88',
+    'TEST':'192.168.25.59'
+]
 node('worker') {
     stage('Action') {
-        cleanWs()
-        writeFile(
-            file: env.BUILD_TAG + '.json',
-            text: JsonOutput.toJson([
-                proto: params.PROTO,
-                port: params.PORT.toInteger(),
-                concurrent: params.CONCURRENT.toInteger(),
-                targets: params.TARGETS.tokenize('\n').sort(),
-                endpoints: params.ENDPOINTS.tokenize('\n').sort()
-            ])
-        )
-        sh "time rmeter --config '${env.BUILD_TAG}.json' --output '${env.BUILD_TAG}.xlsx'"
-        archiveArtifacts(
-            artifacts: env.BUILD_TAG + '.xlsx',
-            followSymlinks: false
+        sh(
+            returnStdout: false,
+            script: "switch.sh --${params.BACKEND} --${params.SWITCH}"
         )
         cleanWs()
     }
