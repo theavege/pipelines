5d544ef10c3f691d6455f9c4cf6698bc8f76fea1
Artem V. Ageev <artem.v.ageev@gmail.com>
2023-03-01 22:45:42 +0300

    goroutines

--- winrm/cleanup.groovy
+++ winrm/cleanup.groovy
@@ -11,7 +11,7 @@ node('worker') {
                     'b8cc5d5da274bddee03c425b6269837e',
                     [
 'Remove-Item C:\\Users\\ivolrt\\AppData\\Local\\Microsoft\\Windows\\INetCache\\IE\\*.js',
-'hostname'
+'Write-Host  "${env:COMPUTERNAME}"'
                     ].join(';')
                 )
                 break;
@@ -19,9 +19,19 @@ node('worker') {
                 winrm(
                     '9354ac3f628b97c899667da482e04e9a',
                     [
-'Stop-ScheduledTask  -TaskName "Restart RT Loader"',
-'Start-ScheduledTask -TaskName "Restart RT Loader"',
-'hostname'
+'Get-Process   -Name "DataProvider"',
+'Stop-Process  -Name "DataProvider" -Force',
+'Get-Process   -Name "RTEngine"',
+'Stop-Process  -Name "RTEngine" -Force',
+'Get-Process   -Name "ActivExecutive"',
+'Stop-Process  -Name "ActivExecutive" -Force',
+'Get-Service   -Name "ActivWorkstationServiceDaemon"',
+'Stop-Service  -Name "ActivWorkstationServiceDaemon" -Force',
+'Start-Service -Name "ActivWorkstationServiceDaemon"',
+'Start-Process -FilePath "C:\\RT\\Services\\RTLoader\DataProvider.exe"'
+'Start-Sleep   -Seconds 10',
+'Start-Process -FilePath "C:\\RT\\Services\\RTLoader\DataProvider.exe"'
+'Write-Host    "${env:COMPUTERNAME}"'
                     ].join(';')
                 )
                 break;
@@ -29,11 +39,13 @@ node('worker') {
                 winrm(
                     '9354ac3f628b97c899667da482e04e9a',
                     [
-'Stop-Service  -Name IVDataService -Force',
-'Get-Process   -Name IVDataService | Stop-Process -Force',
-'Sleep 10',
-'Start-Service -Name IVDataService',
-'hostname'
+'Get-Service   -Name "IVDataService"',
+'Stop-Service  -Name "IVDataService" -Force',
+'Get-Process   -Name "IVDataService"',
+'Stop-Process  -Name "IVDataService" -Force',
+'Start-Sleep   -Seconds 10',
+'Start-Service -Name "IVDataService"',
+'Write-Host    "${env:COMPUTERNAME}"'
                     ].join(';')
                 )
                 break;
