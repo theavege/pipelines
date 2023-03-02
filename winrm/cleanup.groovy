ba14f7172ffb169ead8af1e18f18a1924f2293d1
Artem V. Ageev <artem.v.ageev@gmail.com>
2023-03-02 18:34:17 +0300

    goroutines

--- winrm/cleanup.groovy
+++ winrm/cleanup.groovy
@@ -17,25 +17,24 @@ node('worker') {
                 break;
             case 'dataprovider' :
                 withCredentials([usernamePassword(
-                    credentialsId:    '5ccd3727a9f70e56cbf71e92e3afcc73',
+                    credentialsId:    env.JOB_BASE_NAME,
                     passwordVariable: 'pass',
                     usernameVariable: 'user'
                 )]) {
                     winrm(
                         '9354ac3f628b97c899667da482e04e9a',
                         [
-'Get-Process   -Name "DataProvider"',
-'Stop-Process  -Name "DataProvider" -Force',
-'Get-Process   -Name "RTEngine"',
-'Stop-Process  -Name "RTEngine" -Force',
-'Get-Process   -Name "ActivExecutive"',
-'Stop-Process  -Name "ActivExecutive" -Force',
-'Get-Service   -Name "ActivWorkstationServiceDaemon"',
-'Stop-Service  -Name "ActivWorkstationServiceDaemon" -Force',
-"Start-Service -Name 'ActivExecutive' -ArgumentList '-U${user} -P${pass}'",
-'Start-Process -FilePath "C:\\RT\\Services\\RTLoader\\DataProvider.exe"',
-'Start-Process -FilePath "C:\\RT\\Services\\RTLoader\\DataProvider.exe"',
-'Start-Sleep   -Seconds 10',
+'Get-Process   -Name     "DataProvider"',
+'Stop-Process  -Name     "DataProvider" -Force',
+'Get-Process   -Name     "RTEngine"',
+'Stop-Process  -Name     "RTEngine" -Force',
+'Get-Process   -Name     "ActivExecutive"',
+'Stop-Process  -Name     "ActivExecutive" -Force',
+'Get-Service   -Name     "ActivWorkstationServiceDaemon"',
+'Stop-Service  -Name     "ActivWorkstationServiceDaemon" -Force',
+"Start-Service -Name     'ActivExecutive' -ArgumentList '-U${user} -P${pass}'",
+'Start-Process -FilePath "${env:PROGRAMFILES(X86)}\\ActivWorkstation\\bin\\ActivExecutive.exe"',
+'Start-Sleep   -Seconds  10',
 'Start-Process -FilePath "C:\\RT\\Services\\RTLoader\\DataProvider.exe"',
 'Write-Host    "${env:COMPUTERNAME}"'
                         ].join(';')
@@ -46,12 +45,12 @@ node('worker') {
                 winrm(
                     '9354ac3f628b97c899667da482e04e9a',
                     [
-'Get-Service   -Name "IVDataService"',
-'Stop-Service  -Name "IVDataService" -Force',
-'Get-Process   -Name "IVDataService"',
-'Stop-Process  -Name "IVDataService" -Force',
+'Get-Service   -Name    "IVDataService"',
+'Stop-Service  -Name    "IVDataService" -Force',
+'Get-Process   -Name    "IVDataService"',
+'Stop-Process  -Name    "IVDataService" -Force',
 'Start-Sleep   -Seconds 10',
-'Start-Service -Name "IVDataService"',
+'Start-Service -Name    "IVDataService"',
 'Write-Host    "${env:COMPUTERNAME}"'
                     ].join(';')
                 )
